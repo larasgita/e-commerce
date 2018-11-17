f7f79e28f164113f22828d608a0b7732106dae48
larasgita <44990749+larasgita@users.noreply.github.com>
2018-11-17 12:59:31 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<div class="all">
+	<div class="header1">
+		<div>
+		<a href="index.php">GIRLEE</a>
+		<img src="pict/ig.png">
+		<img src="pict/fb.png">
+		<img src="pict/line.png">
+		</div>
+			Customer Service: 0812 6060 0000
+	</div>
+	<div class="garis"></div>
+
+	<div class="header2">
+		<img src="pict/logo.png">
+
+		<div class="search">
+				<input type="text" name="cari" placeholder="search">
+				<button type="submit" name="submit">Search</button>
+		
+		</div>
+		<div>
+		<a href="login.php">LOGIN/REGISTER</a>
+		<a href="#">MY BAG</a>
+		</div>
+	</div>
---
+++ index.php
@@ -0,0 +1,98 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<?php 			
+include ("header.php");
+?>
+
+<div class="opsi">
+	<a href="#">Shop by department</a>
+	<a href="#">Shop by brand</a>
+</div>
+
+<div class="slider">
+	<figure>
+	<img src="#">
+	<img src="#">
+	<img src="#">
+	</figure>
+	
+</div>
+
+
+<div class="plus" >
+		<div><img src="pict/ori.png"> <p>ORIGINAL PRODUCT</p></div>
+		<div><img src="pict/call.png"><p>FULL CUSTOMER CARE</p></div>
+		<div><img src="pict/click.png"><p>FAST RESPONSE</p></div>
+</div>
+		<div class="gariss"> </div>
+
+
+<h1 align="center"> BESTSELLER</h1>
+	<div class="bestsell">
+
+		<div><img src="pict/produk.png">
+		<p>Nama Produk</p>
+		Penjelasan <br> Rp Harga<br>
+		<a href="#">beli</a>
+		</div>
+
+
+		<div><img src="pict/produk.png">
+		<p>Nama Produk</p>
+		Penjelasan <br> Rp Harga<br>
+		<a href="#">beli</a>
+		</div>
+
+
+		<div><img src="pict/produk.png">
+		<p>Nama Produk</p>
+		Penjelasan <br> Rp Harga<br>
+		<a href="#">beli</a>
+		</div>
+
+
+		<div><img src="pict/produk.png">
+		<p>Nama Produk</p>
+		Penjelasan <br> Rp Harga<br>
+		<a href="#">beli</a>
+		</div>
+	</div>
+
+<div class="gariss"></div>
+
+<h1 align="center">What's New</h1>
+	<div class="bestsell">
+		
+		<div><img src="pict/produk.png">
+		<p>Nama Produk</p>
+		Penjelasan <br> Rp Harga<br>
+		<a href="#">beli</a>
+		</div>
+
+
+		<div><img src="pict/produk.png">
+		<p>Nama Produk</p>
+		Penjelasan <br> Rp Harga<br>
+		<a href="#">beli</a>
+		</div>
+
+
+		<div><img src="pict/produk.png">
+		<p>Nama Produk</p>
+		Penjelasan <br> Rp Harga<br>
+		<a href="#">beli</a>
+		</div>
+
+		
+		<div><img src="pict/produk.png">
+		<p>Nama Produk</p>
+		Penjelasan <br> Rp Harga<br>
+		<a href="#">beli</a>
+		</div>
+	</div>
+
+<div class="gariss"></div>
+
+
+ <?php 
+ include ("end.php");
+  ?>
